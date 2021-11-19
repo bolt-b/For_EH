c0ae42bd5b36f104e57dfdadb5dc49be8c938ccb
bolt-b <bb.sledgehammer.automation@gmail.com>
2021-11-19 21:08:41 +0300

    todays upload

---
+++ ConveyorMaterialAnimationEH/ConveyorMaterialAnimationEH/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,168 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ConveyorMaterialAnimationEH.ViewModel
+{
+    public class MainWindowViewModel: ViewModelBase
+    {
+
+        #region File for properties store
+        private bool _isMaterialsReceiving;
+        private bool _isConveyorRunning;
+        private Portion _currentPortion;
+        private double _speed;
+        private double _conveyorLength;
+        private bool _isMaterialAtEnd;
+        #endregion
+
+        /// <summary>The raw material goes to the tape.</summary>
+        public bool IsMaterialsReceiving
+        {
+            get => _isMaterialsReceiving;
+            set => _ = Set(ref _isMaterialsReceiving, value);
+        }
+
+
+        /// <summary>The conveyor is running.</summary>
+        public bool IsConveyorRunning
+        {
+            get => _isConveyorRunning;
+            set => _ = Set(ref _isConveyorRunning, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            BindingOperations.EnableCollectionSynchronization(Portions, ((ICollection)Portions).SyncRoot);
+            timer = new Timer(OnTimer, null, 10, 10);
+        }
+
+        public ObservableCollection<Portion> Portions { get; }
+            = new ObservableCollection<Portion>();
+
+        public Portion CurrentPortion { get => _currentPortion; private set => Set(ref _currentPortion, value); }
+
+        private readonly Timer timer;
+        private void OnTimer(object state)
+        {
+            if (IsConveyorRunning)
+            {
+                lock (((ICollection)Portions).SyncRoot)
+                {
+                    List<Portion> remove = new List<Portion>();
+                    foreach (Portion portion in Portions)
+                    {
+                        if (portion == CurrentPortion)
+                        {
+                            double length = (DateTime.Now - portion.StartTime).TotalSeconds * Speed;
+                            if (length > ConveyorLength)
+                            {
+                                length = ConveyorLength;
+                            }
+                            portion.Length = length;
+                        }
+                        else
+                        {
+                            double begin = (DateTime.Now - portion.StartTime).TotalSeconds * Speed;
+                            if (begin > ConveyorLength)
+                            {
+                                remove.Add(portion);
+                            }
+                            else
+                            {
+                                portion.Begin = begin;
+                            }
+                        }
+                    }
+                    remove.ForEach(portion => Portions.Remove(portion));
+
+                    IsMaterialAtEnd = Portions.Any(portion => portion.Begin + portion.Length >= ConveyorLength);
+                }
+            }
+        }
+
+        public bool IsMaterialAtEnd { get => _isMaterialAtEnd; set => Set(ref _isMaterialAtEnd, value); }
+        public double Speed { get => _speed; set => Set(ref _speed, value); }
+        public double ConveyorLength { get => _conveyorLength; set => Set(ref _conveyorLength, value); }
+
+        public override void RaisePropertyChanged<T>([CallerMemberName] string propertyName = null, T oldValue = default, T newValue = default, bool broadcast = false)
+        {
+            base.RaisePropertyChanged(propertyName, oldValue, newValue, broadcast);
+
+            if (propertyName == nameof(IsMaterialsReceiving))
+            {
+                lock (((ICollection)Portions).SyncRoot)
+                {
+                    bool isnew = Equals(newValue, true);
+                    if (isnew)
+                    {
+                        Portion portion = new Portion()
+                        {
+                            StartTime = DateTime.Now
+                        };
+                        CurrentPortion = portion;
+                        Portions.Add(portion);
+                    }
+                    else
+                    {
+                        Portion portion = CurrentPortion;
+                        if (portion.Length == 0)
+                        {
+                            Portions.Remove(portion);
+                        }
+                        else
+                        {
+                            portion.StartTime = DateTime.Now;
+                        }
+                        CurrentPortion = null;
+                    }
+                }
+            }
+            else if (propertyName == nameof(IsConveyorRunning))
+            {
+                if (Equals(newValue, true))
+                {
+                    lock (((ICollection)Portions).SyncRoot)
+                    {
+                        foreach (Portion portion in Portions)
+                        {
+                            if (portion == CurrentPortion)
+                            {
+                                portion.StartTime = DateTime.Now.AddSeconds(-portion.Length / Speed);
+                            }
+                            else
+                            {
+                                portion.StartTime = DateTime.Now.AddSeconds(-portion.Begin / Speed);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
+    public class Portion : ViewModelBase
+    {
+        private double _begin;
+        private double _length;
+        private DateTime _startTime;
+
+        public double Begin { get => _begin; set => Set(ref _begin, value); }
+        public double Length { get => _length; set => Set(ref _length, value); }
+
+        public DateTime StartTime { get => _startTime; set => Set(ref _startTime, value); }
+    }
+
+
+}
