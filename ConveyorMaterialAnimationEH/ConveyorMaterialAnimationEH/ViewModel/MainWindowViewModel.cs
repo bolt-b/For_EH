d8107b2252d38201023bafbfedc6f02ecdd83506
Хасп Элд <EldHasp@Gmail.com>
2021-11-21 17:29:01 +0300

    Вариант с независиммым управлением движением конвеера и индикации кнопки.

--- ConveyorMaterialAnimationEH/ConveyorMaterialAnimationEH/ViewModel/MainWindowViewModel.cs
+++ ConveyorMaterialAnimationEH/ConveyorMaterialAnimationEH/ViewModel/MainWindowViewModel.cs
@@ -154,23 +154,11 @@ namespace ConveyorMaterialAnimationEH.ViewModel
                         }
                     }
                 }
+
             }
         }
 
 
-    }
-
-
-    public class Portion : ViewModelBase
-    {
-        private double _begin;
-        private double _length;
-        private DateTime _startTime;
-
-        public double Begin { get => _begin; set => Set(ref _begin, value); }
-        public double Length { get => _length; set => Set(ref _length, value); }
-
-        public DateTime StartTime { get => _startTime; set => Set(ref _startTime, value); }
     }
 
 
---
+++ ConveyorMaterialAnimationEH/ConveyorMaterialAnimationEH/ViewModel/Portion.cs
@@ -0,0 +1,19 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+
+namespace ConveyorMaterialAnimationEH.ViewModel
+{
+    public class Portion : ViewModelBase
+    {
+        private double _begin;
+        private double _length;
+        private DateTime _startTime;
+
+        public double Begin { get => _begin; set => Set(ref _begin, value); }
+        public double Length { get => _length; set => Set(ref _length, value); }
+
+        public DateTime StartTime { get => _startTime; set => Set(ref _startTime, value); }
+    }
+
+
+}
